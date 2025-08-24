3126d38f4d0a810c0c6e5742375f1eeeb19fc722
MoonFeather <moonbite233@gmail.com>
2025-08-24 12:44:53 +0800

    add json library

---
+++ cmake/Findjson.cmake
@@ -0,0 +1,5 @@
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+    cmake_policy(SET CMP0135 NEW)
+endif()
+FetchContent_Declare(json URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz)
+FetchContent_MakeAvailable(json)
