dd426d3152b56d9a801aad2b14ebf883b78093b0
WindWeaver <moonbite233@gmail.com>
2026-04-11 01:41:31 +0800

    print referee data in real_comm

--- samples/real_comm/main.cpp
+++ samples/real_comm/main.cpp
@@ -1,48 +1,50 @@
 #include <HySerial/HySerial.hpp>
 #include <RPL/Deserializer.hpp>
-#include <RPL/Packets/RoboMaster/CustomControllerData.hpp>
-#include <RPL/Packets/Sample/SampleA.hpp>
-#include <RPL/Packets/Sample/SampleB.hpp>
-#include <RPL/Packets/VT03RemotePacket.hpp>
+#include <RPL/Packets/RoboMaster/HurtData.hpp>
+#include <RPL/Packets/RoboMaster/PowerHeatData.hpp>
+#include <RPL/Packets/RoboMaster/RobotStatus.hpp>
 #include <RPL/Parser.hpp>
-#include <RPL/Serializer.hpp>
 #include <algorithm>
 #include <format>
 #include <iostream>
-#include <ranges>
 #include <span>
 
-std::string print_remote(const VT03RemotePacket &packet) {
-  return std::format("x1: {}, y1: {}, x2: {}, y2: {}", packet.left_stick_x,
-                     packet.left_stick_y, packet.right_stick_x,
-                     packet.right_stick_y);
+std::string print_packet(const HurtData &packet) {
+  return std::format("HurtData: armor_id={}, hp_deduction_reason={}",
+                     packet.armor_id, packet.hp_deduction_reason);
 }
 
-std::string print_sample_a(const SampleA &sample) {
-  auto &[a, b, c, d] = sample;
-  return std::format("a: {} b: {} c: {} d: {}", a, b, c, d);
+std::string print_packet(const PowerHeatData &packet) {
+  return std::format(
+      "PowerHeatData: buffer_energy={}, shooter_17mm_1_barrel_heat={}, "
+      "shooter_42mm_barrel_heat={}",
+      packet.buffer_energy, packet.shooter_17mm_1_barrel_heat,
+      packet.shooter_42mm_barrel_heat);
 }
 
-std::string print_cc(const CustomControllerData &data) {
-  return std::format("CC: {}, {}, {}, {}...", data.data[0], data.data[1],
-                     data.data[2], data.data[3]);
-}
-
-std::string print_sample_b(const SampleB &sample) {
-  auto &[x, y] = sample;
-  return std::format("x: {}, y: {}", x, y);
+std::string print_packet(const RobotStatus &packet) {
+  return std::format(
+      "RobotStatus: robot_id={}, robot_level={}, current_hp={}, "
+      "maximum_hp={}, cooling_value={}, heat_limit={}, "
+      "chassis_power_limit={}, gimbal_output={}, chassis_output={}, "
+      "shooter_output={}",
+      packet.robot_id, packet.robot_level, packet.current_hp,
+      packet.maximum_hp, packet.shooter_barrel_cooling_value,
+      packet.shooter_barrel_heat_limit, packet.chassis_power_limit,
+      packet.power_management_gimbal_output,
+      packet.power_management_chassis_output,
+      packet.power_management_shooter_output);
 }
 
 static constexpr auto DEVICE_PATH = "/dev/ttyUSB0";
 
 int main() {
-  RPL::Deserializer<CustomControllerData, VT03RemotePacket> deserializer;
-  RPL::Parser<CustomControllerData, VT03RemotePacket> parser{deserializer};
-  RPL::Serializer<SampleB> serializer;
+  RPL::Deserializer<HurtData, PowerHeatData, RobotStatus> deserializer;
+  RPL::Parser<HurtData, PowerHeatData, RobotStatus> parser{deserializer};
 
   HySerial::Builder builder;
   builder.device(DEVICE_PATH)
-      .baud_rate(921600)
+      .baud_rate(115200)
       .data_bits(HySerial::DataBits::BITS_8)
       .parity(HySerial::Parity::NONE)
       .stop_bits(HySerial::StopBits::ONE);
@@ -64,27 +66,16 @@ int main() {
   auto serial = std::move(serial_or_err.value());
 
   serial->start_read();
-  SampleB packet_send{};
 
-  uint8_t s = 0;
   while (true) {
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    auto &[x, y] = packet_send;
-    x += 1;
-    y += 2;
-    std::array<uint8_t, 1024> buffer{};
-    auto res = serializer.serialize(buffer.data(), buffer.size(), packet_send);
-    if (!res) {
-      std::cerr << res.error().message << std::endl;
-      continue;
-    }
-    serial->send({std::span(reinterpret_cast<const std::byte *>(buffer.data()),
-                            res.value())});
-    auto cc_packet = deserializer.get<CustomControllerData>();
-    auto vt_packet = deserializer.get<VT03RemotePacket>();
-    std::cout << print_cc(cc_packet) << std::endl;
-    std::cout << print_remote(vt_packet) << std::endl;
-    // std::cout << print_sample_b(packet_send) << std::endl;
-    s++;
+    
+    auto hurt_data = deserializer.get<HurtData>();
+    auto power_heat = deserializer.get<PowerHeatData>();
+    auto robot_status = deserializer.get<RobotStatus>();
+    
+    std::cout << print_packet(hurt_data) << std::endl;
+    std::cout << print_packet(power_heat) << std::endl;
+    std::cout << print_packet(robot_status) << std::endl;
   }
 }
